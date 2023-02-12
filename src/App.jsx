08b6331961837eed202d6631e880fa2951506719
Puskass <cosovictarik711@gmail.com>
2023-02-12 09:40:06 +0100

    data structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-center text-5xl font-bold underline text-green-700">
       Hello world!
     </h1>
   );
